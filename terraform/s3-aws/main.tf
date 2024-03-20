c2b00ad9b90fa85fcdae5abbce8619ca3a85f68c
Venkat Padmanabhan <venkatakrishnan.padmanabhan@gmail.com>
2024-03-20 09:26:05 -0400

    Update main.tf

--- terraform/s3-aws/main.tf
+++ terraform/s3-aws/main.tf
@@ -10,9 +10,9 @@ provider "aws" {
   region = "us-east-1"
 
   # Make it faster by skipping something
-  skip_metadata_api_check     = true
-  skip_region_validation      = true
-  skip_credentials_validation = true
+  # skip_metadata_api_check     = true
+  # skip_region_validation      = true
+  # skip_credentials_validation = true
 
   default_tags {
     tags = {
